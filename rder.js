2d918f303eb5918fe2ec223f185ab28e4ef6fc38
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-15 16:08:32 +0800

    Updated client test.

--- rder.js
+++ rder.js
@@ -35,6 +35,10 @@
 			"module": "rder",
 			"author": "Richeve S. Bebedor",
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/rder.git",
 			"test": "rder-test.js",
 			"global": true
