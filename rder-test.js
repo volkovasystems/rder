5eced06d8633b4ed0c5c5eadb4945bee4274808f
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-07 15:53:29 +0800

    Updated server/client test.

--- rder-test.js
+++ rder-test.js
@@ -2,9 +2,6 @@
 const assert = require( "assert" );
 const rder = require( "./rder.js" );
 
-assert.deepEqual( rder( [ Array, RegExp, Object ], "name" ),
-	{ "Array": 0, "RegExp": 1, "Object": 2 }, "should be deeply equal" );
-
 assert.deepEqual( rder( [ 1, 2, 3 ], "name" ),
 	{ "1": 0, "2": 1, "3": 2 }, "should be deeply equal" );
 
